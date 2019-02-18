9b24d2023c8d877b3e1963699c87b326003e5d11
Christian Hammervig <christian@hammervig.dk>
2019-02-18 15:18:14 +0100

    Update signin.js

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,4 +1,4 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
+const handleSignin = (req, res, db, bcrypt) => {
 	const { email, password } = req.body;
 	if (!email || !password) {
 		return res.status(400).json('incorrect form submission');
@@ -6,6 +6,7 @@ const handleSignin = (db, bcrypt) => (req, res) => {
 	db.select('email', 'hash').from('login')
 		.where('email', '=', email)
 		.then(data => {
+			console.log(data);
 			const isValid = bcrypt.compareSync(password, data[0].hash);
 			if (isValid) {
 				return db.select('*').from('users')
