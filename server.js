0b7ab2dad0da0c139801027a8572609555b92415
Christian Hammervig <christian@hammervig.dk>
2019-02-22 13:11:06 +0100

    Update server.js
    corrected imageurl from put to post

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 
-app.put('/imageUrl', (req, res) => {image.handleApiCall(req, res)})
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 8080, () => {
 	console.log(`app is runing on port 8080`)
